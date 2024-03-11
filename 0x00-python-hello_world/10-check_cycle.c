62b1e5d06d21188048d225f53532fd6c74adce6e
adisu-mulu <kulhabesh31@gmail.com>
2024-03-11 20:46:30 +0300

    ading 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<lists.h>
+#include<stdlib.h>
+/**
+ * check_cycle - function
+ * @list: pointer
+ * Return: 0 or 1
+ */
+
+
+int check_cycle(listint_t *list)
+{
+	listint_t *temp = list;
+	while (list->next != NULL && temp->next->next != NULL)
+	{
+		if(list->next == temp->next)
+			return (1);
+		else
+		{
+			list = list->next;
+			temp = temp->next;
+		}
+	}
+	reurn (1);
+}
