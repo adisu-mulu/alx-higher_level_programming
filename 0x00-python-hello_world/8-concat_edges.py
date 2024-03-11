c232b78fe61e891f68a58e6222a3cf6d5bf76365
adisu-mulu <kulhabesh31@gmail.com>
2024-03-11 17:18:28 +0300

    modifying 8-concat

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
-language that combines remarkable power with very clear syntax"
-str = "object-oriented programming with " + str[str.index("Python"):].split()[0]
+ language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[106:112] + str[:6]
 print(str)
