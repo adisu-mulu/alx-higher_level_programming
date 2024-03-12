8e827cb5e94c43e504ffc6456452aa9610bf7a9d
adisu-mulu <kulhabesh31@gmail.com>
2024-03-12 16:29:13 +0300

    solved pycodestyle

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 for num in range(0, 100):
-   if num < 10:
-      strnum = "0" + str(num)
-   else:
-      strnum = str(num)
-   if num < 99:
-      print("{}".format(strnum), end=", ")
-   else:
-      print("{}".format(strnum))
-
+    if num < 10:
+        strnum = "0" + str(num)
+    else:
+        strnum = str(num)
+    if num < 99:
+        print("{}".format(strnum), end=", ")
+    else:
+        print("{}".format(strnum))
