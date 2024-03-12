d79941be7b532e4e3ddf0303dbe06edbda246d4e
adisu-mulu <kulhabesh31@gmail.com>
2024-03-12 23:40:59 +0300

    updating 100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,5 +3,5 @@ i = 0
 letters = ""
 for c in range(ord('z'), ord('a') - 1, -1):
     letters += chr(c - i)
-    i =32 if i == 0 else 0
-print(letters)
+    i = 32 if i == 0 else 0
+print("{}".format(letters))
