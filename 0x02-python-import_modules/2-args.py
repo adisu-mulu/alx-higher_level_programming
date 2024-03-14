09843a2d98f5c6dfc0c6f82badf0782611b5cade
adisu-mulu <kulhabesh31@gmail.com>
2024-03-14 14:28:12 +0300

    updating 2-args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import sys
-print("{} arguements:".format(len(sys.argv)-1))
-x = 0
-for arg in sys.argv[1:]:
-    x += 1
-    print("{}: {}".format(x, arg))
+if __name__ == "__main__":
+    import sys
+    print("{} arguements:".format(len(sys.argv)-1))
+    x = 0
+    for arg in sys.argv[1:]:
+        x += 1
+        print("{}: {}".format(x, arg))
 
