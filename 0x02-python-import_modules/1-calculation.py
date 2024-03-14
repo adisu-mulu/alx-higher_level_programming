da3ba03f70b39f1350272ecdfd6efa34c15824dd
adisu-mulu <kulhabesh31@gmail.com>
2024-03-14 14:05:05 +0300

    updating 1-calc

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import 1-calculation
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, 1-calculation.add(a, b)))
-print("{} + {} = {}".format(a, b, 1-calculation.sub(a, b)))
-print("{} + {} = {}".format(a, b, 1-calculation.mul(a, b)))
-print("{} + {} = {}".format(a, b, 1-calculation.div(a, b)))
+if __name__ == "__main_":
+    import 1-calculation
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, 1-calculation.add(a, b)))
+    print("{} + {} = {}".format(a, b, 1-calculation.sub(a, b)))
+    print("{} + {} = {}".format(a, b, 1-calculation.mul(a, b)))
+    print("{} + {} = {}".format(a, b, 1-calculation.div(a, b)))
