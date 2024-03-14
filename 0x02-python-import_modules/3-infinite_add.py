bc4d5ffd37c4dd381c3948842135078196d2bdee
adisu-mulu <kulhabesh31@gmail.com>
2024-03-14 14:45:42 +0300

    3-inf

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-import sys
-sum = 0
-for arg in sys.argv[1:]:
-    sum += int(arg)
-print("{}".format(sum))
+if __name__ == "__main__":
+    import sys
+    sum = 0
+    for arg in sys.argv[1:]:
+        sum += int(arg)
+    print("{}".format(sum))
 
