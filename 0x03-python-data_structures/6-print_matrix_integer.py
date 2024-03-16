98e54587ba61c2d1d43d3b1503a99e6980406ff1
adisu-mulu <kulhabesh31@gmail.com>
2024-03-16 21:40:17 +0300

    updating 6-p

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,10 @@
 def print_matrix_integer(matrix=[[]]):
     if matrix:
         for x in matrix:
+            count = 0
             for y in x:
-                print("{:d}".format(y), end=' ')
+                print("{:d}".format(y), end='')
+                count += 1
+                if count != len(x):
+                    print(" ", end='')
             print()
