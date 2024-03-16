798cc0d7ded182b2c2abddbf4ce477c5482e7c1f
adisu-mulu <kulhabesh31@gmail.com>
2024-03-16 21:13:28 +0300

    5-no

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    x = 0
+    newstring = [] 
+    for val in my_string:
+        if val == 'c' or val == 'C':
+            continue
+        else:
+            newstring.append(val)
+    return ''.join(newstring)
