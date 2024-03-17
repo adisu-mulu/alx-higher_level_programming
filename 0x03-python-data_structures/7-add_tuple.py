527bffb8c719b4e72ed24779713ba21e7c8fb5e3
adisu-mulu <kulhabesh31@gmail.com>
2024-03-17 10:46:15 +0300

    adding 7-

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    a = list(tuple_a)
+    b = list(tuple_b)
+    while len(a) < 2:
+        a.append(0)
+    while len(b) < 2:
+        b.append(0)
+    while len(a) > 2:
+        a.remove[-1]
+    while len(b) > 2:
+        b.remove[-1]
+
+    new_tuple = (a[0] + b[0], a[1] + b[1])
+    return new_tuple
+        
+            
