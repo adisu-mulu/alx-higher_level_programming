eba64abb3ad6fc458fa70f67eb7e9457bc6827d6
adisu-mulu <kulhabesh31@gmail.com>
2024-03-18 11:58:26 +0300

    upding 100-p

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -7,11 +7,12 @@
 
 void print_python_list_info(PyObject *p)
 {
-	Py_ssize_t size = PyList_Size(p);
-	Py_ssize_t refCount = Py_REFCNT(p);
-	PyObject *type = Py_TYPE(p);
+	long int size = PyList_Size(p);
+	int i;
+	PyListObject *obj = (PyListObject *)p;
 
-	printf("Size of the list: %zd\n", size);
-	printf("Reference count: %zd\n", refCount);
-	printf("Type of the object: %s\n", Py_TYPE(p)->tp_name);
+	printf("[*] Size of the Python List = %li\n", size);
+	printf("[*] Allocated = %li\n", obj->allocated);
+	for (i = 0; i < size; i++)
+		printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
 }
