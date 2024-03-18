a54b1bd1fa225aaf7b2eb28d49a8ec76c57a4414
adisu-mulu <kulhabesh31@gmail.com>
2024-03-18 12:03:34 +0300

    upding 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -6,14 +6,14 @@
  */
 int is_palindrome(listint_t **head)
 {
-	if (*head == NULL || (*head)->next == NULL)
-		return (1);
 	listint_t *temp;
-
 	listint_t *slow = *head;
 	listint_t *fast = *head;
 	listint_t *prev = NULL;
 
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
+	
 	while (fast != NULL && fast->next != NULL)
 	{
 		fast = fast->next->next;
