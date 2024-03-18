af672a01e0558e9502c0638c6dcfcf8a8663b603
adisu-mulu <kulhabesh31@gmail.com>
2024-03-18 11:38:49 +0300

    adding 100-p

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,20 @@
+#include <listobject.h>
+#include <object.h>
+#include "lists.h"
+
+/**
+ * print_python_list_info - function
+ * @p: object
+ * Return: nothing
+ */
+
+void print_python_list_info(PyObject *p)
+{
+	Py_ssize_t size = PyList_Size(p);
+	Py_ssize_t refCount = Py_REFCNT(p);
+	PyObject *type = Py_TYPE(p);
+
+	printf("Size of the list: %zd\n", size);
+	printf("Reference count: %zd\n", refCount);
+	printf("Type of the object: %s\n", Py_TYPE(p)->tp_name);
+}
