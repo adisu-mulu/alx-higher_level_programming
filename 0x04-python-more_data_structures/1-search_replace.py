bb69d2158dda3e81e1082f9062d441499abd127c
adisu-mulu <kulhabesh31@gmail.com>
2024-03-20 17:12:18 +0300

    updating 1-search

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    newlist=[]
+    newlist = []
     for var in my_list:
         if var == search:
             newlist.append(replace)
         else:
             newlist.append(var)
     return newlist
-            
