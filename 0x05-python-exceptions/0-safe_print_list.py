b4564ebf6a150b5fe5fa27de99010c9e74ed9398
adisu-mulu <kulhabesh31@gmail.com>
2024-04-01 14:25:05 +0300

    updating 0-safe

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,12 +3,9 @@ def safe_print_list(my_list=[], x=0):
     n = 0
     try:
         for i in range(x):
-            print(i, end="")
+            print(my_list[i], end="")
             n = n+1
         print("\n")
-        return (n)
     except IndexError:
         print("\n")
-        return (n)
-    
-    
+    return (n)
