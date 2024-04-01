887a09baa795002be2e66d4567aa5211ecf94568
adisu-mulu <kulhabesh31@gmail.com>
2024-04-01 13:55:04 +0300

    updating .py

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    n = 0
+    count = 0
+    try:
+        while (n < x):
+            if(my_list[n].__class__ == int):
+               print("{:d}".format(my_list[n]), end="")
+               n+=1
+               count+=1
+            else:
+               n+=1
+        print("\n")
+        return (count)
+    except IndexError:
+            return (IndexError)
+    
