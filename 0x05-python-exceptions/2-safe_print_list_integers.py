dbe4b1cb3df8732ae122c84565376361c417ed23
adisu-mulu <kulhabesh31@gmail.com>
2024-04-01 20:58:42 +0300

    updating 2-safe indexrrr

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -12,5 +12,5 @@ def safe_print_list_integers(my_list=[], x=0):
                 n += 1
         print()
         return (count)
-    except Exception:
-        pass
+    except (ValueError, TypeError) as ie:
+        return (ie)
