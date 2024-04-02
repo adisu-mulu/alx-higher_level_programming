869fc0e01b9b2a4e716a575bf3a0afe283dbe808
adisu-mulu <kulhabesh31@gmail.com>
2024-04-02 17:50:59 +0300

    updating 3square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -4,6 +4,8 @@
 
 class Square:
     """This class defines the init method to initialize size"""
+
+
     def __init__(self, size=0):
         """the init method initializes size as private instance attribute
         Args:
