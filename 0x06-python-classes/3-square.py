f51b2add7335d3accd46f505955b928b819fbc75
adisu-mulu <kulhabesh31@gmail.com>
2024-04-02 22:28:41 +0300

    updating 3-sq

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 
 
-"""This module defines a class Square"""
+"""This module defines a class Square."""
 
 
 class Square:
-    """This class defines the init method to initialize size"""
+    """This class defines the init method to initialize size."""
 
     def __init__(self, size=0):
-        """the init method initializes size as private instance attribute
+        """the init method initializes size as private instance attribute.
         Args:
-            size (int): the size of square
+            size (int): the size of square.
         Attributes:
-            __size (int): private variable that stores size
+            __size (int): private variable that stores size.
         """
         if size.__class__ != int:
             raise TypeError("size must be an integer")
@@ -21,5 +21,5 @@ class Square:
         self.__size = size
 
     def area(self):
-        """Return the computes the square of the size"""
+        """Return the computes the square of the size."""
         return(self.__size * self.__size)
