e1691b5bde370c7d1ec202eb7658a5f38929f3f1
adisu-mulu <kulhabesh31@gmail.com>
2024-04-02 15:55:53 +0300

    adding 4-square

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
 """This module defines a class Square"""
+
+
 class Square:
     """This class defines the init method to initialize size"""
     def __init__(self, size=0):
         """the init method initializes size as private instance attribute"""
         self.__size = size
 
-
+    @property
     def size(self):
+        """Thsi method retrieves the value of size"""
         return (self.__size)
-    
+
+    @size.setter
     def size(self, value):
+        """This method sets the value of size"""
         if (value.__class__ != int):
             raise TypeError("size must be an integer")
         if (value < 0):
@@ -18,4 +23,5 @@ class Square:
         self.__size = value
         
     def area(self):
-        return(self.__size **2)
+        """The method calculates the area of size"""
+        return(self.__size ** 2)
