976c669b64c7e730b9347662e821bd78663f26c4
adisu-mulu <kulhabesh31@gmail.com>
2024-04-02 16:58:39 +0300

    updating 3-sqrs

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -5,7 +5,10 @@
 class Square:
     """This class defines the init method to initialize size"""
     def __init__(self, size=0):
-        """the init method initializes size as private instance attribute"""
+        """the init method initializes size as private instance attribute
+        Args:
+             size (int): the size of square
+        """
         if (size.__class__ != int):
             raise TypeError("size must be an integer")
         if (size < 0):
