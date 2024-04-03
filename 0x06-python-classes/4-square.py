b523f2d6c77402b2c4af581b4476402ee30204a2
adisu-mulu <kulhabesh31@gmail.com>
2024-04-03 15:23:15 +0300

    updating 4-sq

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -21,7 +21,7 @@ class Square:
         if (value < 0):
             raise ValueError("size must be >= 0")
         self.__size = value
-        
+
     def area(self):
         """The method calculates the area of size"""
-        return(self.__size ** 2)
+        return (self.__size ** 2)
