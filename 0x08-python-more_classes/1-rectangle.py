bd022e2ff6225fb763e6bc5f4f7573947303be6b
adisu-mulu <kulhabesh31@gmail.com>
2024-04-08 22:48:37 +0300

    updating 1-rec

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -9,20 +9,30 @@ class Rectangle:
     Class Rectangle implements the init function
     """
     def __init__(self, width=0, height=0):
-        """ the init function initializes a new Rectangle object """
+        """ the init function initializes a new Rectangle object
+        Args:
+        width (int): width of the rectangle
+        height (int): height of the rectangle
+        """
         self.height = height
         self.width = width
 
     @property
     def width(self):
+        """ Returns the width of the rectangle"""
         return (self.__width)
 
     @property
     def height(self):
+        """Returns the height of the rectangle"""
         return (self.__height)
 
     @width.setter
     def width(self, value):
+        """sets the width of the rectangle
+        Args:
+        value (int): the new value
+        """
         if type(value) not in [int]:
             raise TypeError("width mut be an integer")
         if value < 0:
@@ -31,6 +41,10 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
+        """sets the height of the rectangle
+        Args:
+        value (int): the new value
+        """
         self.__height = value
         if type(value) not in [int]:
             raise TypeError("height mut be an integer")
