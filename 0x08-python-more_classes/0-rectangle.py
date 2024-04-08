f92eea3715c80774b676f90de35ef0cf07767067
adisu-mulu <kulhabesh31@gmail.com>
2024-04-08 15:22:42 +0300

    adding 0x08

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module defines a class called Rectangle
+"""
+
+
+class Rectangle:
+    """Class Rectangle implements the init function to initialize a rectangle object"""
+    
+    def __inint__(self):
+        """ the init function initializes a new Rectangle object """
+        pass
