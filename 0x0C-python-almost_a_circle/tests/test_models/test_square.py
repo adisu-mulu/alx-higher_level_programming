c2ddcd0aff5994e7934dd34140647e3c394d28d2
adisu-mulu <kulhabesh31@gmail.com>
2024-04-25 22:14:35 +0300

    updating test_square to include to_dict..

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -23,3 +23,28 @@ class TestSquare(unittest.TestCase):
         print(s1)
         output = captured_output.getvalue()
         self.assertIn("[Square] (2) 0/0 - 5", output)
+
+    def test_update(self):
+        """This function test the cases for updation"""
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        s1 = Square(5)
+        s1.update(10)
+        s1.update(1, 2)
+        s1.update(1, 2, 3)
+        s1.update(1, 2, 3, 4)
+        s1.update(x=12)
+        s1.update(size=7, y=1)
+        s1.update(size=7, id=89, y=1)
+        print(s1)
+        output = captured_output.getvalue()
+        self.assertIn("[Square] (89) 12/1 - 7", output)
+
+    def test_to_dictioary(self):
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        s1 = Square(10, 2, 1)
+        s1_dictionary = s1.to_dictionary()
+        print(type(s1_dictionary))
+        output = captured_output.getvalue()
+        self.assertIn("<class 'dict'>", output)
