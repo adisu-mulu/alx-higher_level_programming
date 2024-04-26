63916f758d914961cacdf16c9c790342a42992c7
adisu-mulu <kulhabesh31@gmail.com>
2024-04-26 10:44:34 +0300

    updating base with json_to_string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ This module defines the base class for the project"""
+import json
 
 
 class Base:
@@ -16,3 +17,10 @@ class Base:
         else:
             Base.__nb_objects = Base.__nb_objects + 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """This method converts python objects to json strings"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return (json.dumps(list_dictionaries))
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """ This module is used to test the base class"""
 from models.base import Base
-
+from models.rectangle import Rectangle
 import unittest
+import io
+import sys
 
 
 class TestBase(unittest.TestCase):
@@ -17,3 +19,13 @@ class TestBase(unittest.TestCase):
         b2 = Base()
         self.assertEqual(Base._Base__nb_objects, 1)
         self.assertEqual(b2.id, Base._Base__nb_objects)
+
+    def test_json_to_string(self):
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        r1 = Rectangle(10, 7, 2, 8)
+        dictionary = r1.to_dictionary()
+        json_dictionary = Base.to_json_string([dictionary])
+        print(type(json_dictionary))
+        output = captured_output.getvalue()
+        self.assertIn("<class 'str'>", output)
