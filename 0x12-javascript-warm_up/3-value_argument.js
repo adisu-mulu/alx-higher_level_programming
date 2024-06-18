3563f567c36621f6d25d80e7f27d2263f9b443c1
adisu-mulu <kulhabesh31@gmail.com>
2024-06-18 21:12:42 +0300

    modified 3-argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 
-if (argv.length - 2 === 0) {
+if (argv[2] === undefined) {
   console.log('No argument');
-} else if (argv.length - 2 === 1) {
-  console.log(argv[2]);
 } else {
-  console.log('Arguments found');
+  console.log(argv[2]);
 }
