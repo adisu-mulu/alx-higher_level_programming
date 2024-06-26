71183ad1799b9dae33b201146537184b08545244
adisu-mulu <kulhabesh31@gmail.com>
2024-06-26 15:57:00 +0300

    fixed executing when imported

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,12 +3,13 @@
 import sys
 import MySQLdb
 
-db = MySQLdb.connect(
-    user=sys.argv[1], passwd=sys.argv[2],
-    db=sys.argv[3], host="localhost", port=3306)
-cursor = db.cursor()
-cursor.execute("select * from states where name like 'N%'
-               order by states.id ASC")
-result = cursor.fetchall()
-for row in result:
-    print(row)
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        user=sys.argv[1], passwd=sys.argv[2],
+        db=sys.argv[3], host="localhost", port=3306)
+    cursor = db.cursor()
+    cursor.execute("""select * from states where name like 'N%'
+                    order by states.id ASC""")
+    result = cursor.fetchall()
+    for row in result:
+        print(row)
