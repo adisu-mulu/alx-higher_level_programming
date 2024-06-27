1a17921dbdf9d9d4310c6958d9fa1c3c8e49f497
adisu-mulu <kulhabesh31@gmail.com>
2024-06-27 15:14:57 +0300

    corrected check 8

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -8,10 +8,9 @@ if __name__ == '__main__':
         user=sys.argv[1], passwd=sys.argv[2],
         db=sys.argv[3], host="localhost", port=3306)
     cursor = db.cursor()
-    cursor.execute("""select cities.name from cities inner join states on states.id = cities.state_id
-                      and states.name like %s""",(sys.argv[4],))
-
-
+    cursor.execute("""select cities.name from cities inner join
+                      states on states.id = cities.state_id
+                      and states.name like %s""", (sys.argv[4],))
     result = cursor.fetchall()
     formatted_result = [row[0] for row in result]
     output = ', '.join(formatted_result)
